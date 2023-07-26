f050500305c74d8efc5eea6eb4640ac81f3ec915
nisanursrc <nisanurrsrc4@gmail.com>
2023-07-26 20:43:15 +0300

    Soru 2

--- app/src/main/java/com/example/uygulama2/Otomobil.kt
+++ app/src/main/java/com/example/uygulama2/Otomobil.kt
@@ -1,7 +1,7 @@
 package com.example.uygulama2
 
 class Otomobil(hiz: Double, yakitTuketimi: Double, val bagajHacmi: Double) : Arac(hiz, yakitTuketimi) {
-    // Bagaj aç metodu
+
     fun bagajAc() {
         println("Bagaj açılıyor...")
     }
